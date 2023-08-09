12aef54585ccdebc426a2b9011d84b3259b252fc
Bankmock <133338375+Bankmock@users.noreply.github.com>
2023-08-10 04:10:59 +0530

    Update r.js

--- mock3/r.js
+++ mock3/r.js
@@ -166,7 +166,7 @@
                     
                     
                 ],
-                "correctAnswers": [1, 0, 0] // Index of correct answer for each question
+                "correctAnswers":   [1, 3, 2, 0, 2, 1, 3, 4, 2, 3, 0, 0, 1, 2, 1, 3, 4, 0, 2, 2, 1, 2, 3, 0, 3, 2, 0, 4, 3, 0, 1, 1, 2, 0, 4]  // Index of correct answer for each question
             }
         };
 
